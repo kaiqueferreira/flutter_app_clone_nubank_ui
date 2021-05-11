5729c87f1d63f78fdcbcf3e4e5da727428c04cf1
kfappgit <kfappgithub@gmail.com>
2021-05-11 18:02:58 -0300

    commit 2

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -12,9 +12,11 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   bool _showMenu;
   int _currentIndex;
+  double _yPosition;
 
   @override
   void initState() {
+    super.initState();
     //State initial of page
     _showMenu = false;
     _currentIndex = 0;
@@ -23,6 +25,9 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     double _screenHeight = MediaQuery.of(context).size.height;
+    if (_yPosition == null) {
+      _yPosition = _screenHeight * 0.24;
+    }
     // TODO: implement build
     return Scaffold(
       backgroundColor: Colors.purple[800],
@@ -35,22 +40,63 @@ class _HomePageState extends State<HomePage> {
               onTap: () {
                 setState(() {
                   _showMenu = !_showMenu;
+                  _yPosition =
+                      _showMenu ? _screenHeight * .75 : _screenHeight + .24;
                 });
               }),
           PageViewApp(
-            top: _screenHeight * 0.24,
+            showMenu: _showMenu,
+            //Show menu tap go top bottom, back go to top
+            top: _yPosition,
+            //!_showMenu ? _screenHeight * 0.24 : _screenHeight * 0.75,
             // Send index page changed
             onChanged: (index) {
               setState(() {
                 _currentIndex = index;
               });
             },
+            onPanUpdate: (details) {
+              // Automatic border limit move
+              double positionBottomLimit = _screenHeight * .75;
+              double positionTopLimit = _screenHeight * .24;
+              double midlePosition = positionBottomLimit - positionTopLimit;
+              midlePosition = midlePosition / 2;
+
+              setState(() {
+                _yPosition += details.delta.dy;
+
+                _yPosition = _yPosition < positionTopLimit
+                    ? positionTopLimit
+                    : _yPosition;
+
+                _yPosition = _yPosition > positionBottomLimit
+                    ? positionBottomLimit
+                    : _yPosition;
+
+                if (_yPosition != positionBottomLimit && details.delta.dy > 0) {
+                  _yPosition =
+                      _yPosition > positionTopLimit + midlePosition - 50
+                          ? positionBottomLimit
+                          : _yPosition;
+                }
+
+                if (_yPosition != positionTopLimit && details.delta.dy < 0) {
+                  _yPosition = _yPosition < positionBottomLimit - midlePosition
+                      ? positionTopLimit
+                      : _yPosition;
+                }
+
+                if (_yPosition == positionBottomLimit) {
+                  _showMenu = true;
+                } else if (_yPosition == positionTopLimit) {
+                  _showMenu = false;
+                }
+              });
+            },
           ),
-          Positioned(
-            top: _screenHeight * 0.70,
-            child: MyDotsApp(
-              currentIndex: _currentIndex,
-            ),
+          MyDotsApp(
+            top: _screenHeight * .70,
+            currentIndex: _currentIndex,
           ),
         ],
       ),
--- lib/pages/home/widgets/page_view_app.dart
+++ lib/pages/home/widgets/page_view_app.dart
@@ -4,30 +4,42 @@ import 'package:flutter_app_clone_nubank_ui/pages/home/widgets/card_app.dart';
 class PageViewApp extends StatelessWidget {
   final double top;
   final ValueChanged<int> onChanged;
+  final GestureDragUpdateCallback onPanUpdate;
+  final bool showMenu;
 
-  const PageViewApp({Key key, this.top, this.onChanged}) : super(key: key);
+  const PageViewApp(
+      {Key key, this.top, this.onChanged, this.onPanUpdate, this.showMenu})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Positioned(
+    return AnimatedPositioned(
+      duration: Duration(milliseconds: 300),
+      curve: Curves.easeOut,
       top: top,
       height: MediaQuery.of(context).size.height * 0.45,
       left: 0,
       right: 0,
       //width: MediaQuery.of(context).size.width,
-      child: PageView(
-        //onPageChanged
-        // Index of page changed
-        onPageChanged: onChanged,
-        // PageView used by equals ScrollView contain Cards
-        // physics BouncingScrollPhysics used by create effect view
-        physics: BouncingScrollPhysics(),
-        children: [
-          CardApp(),
-          CardApp(),
-          CardApp(),
-        ],
+      child: GestureDetector(
+        onPanUpdate: onPanUpdate,
+        child: PageView(
+          //onPageChanged
+          // Index of page changed
+          onPageChanged: onChanged,
+          // PageView used by equals ScrollView contain Cards
+          // physics BouncingScrollPhysics used by create effect view
+          //If card stay in bottom NeverScrollableScrollPhysics dont permission scroll
+          physics: showMenu
+              ? NeverScrollableScrollPhysics()
+              : BouncingScrollPhysics(),
+          children: [
+            CardApp(),
+            CardApp(),
+            CardApp(),
+          ],
+        ),
       ),
     );
   }
